4ad56437466e100f90fff1e300aea83c1f12e325
AnitaIobidze <anita.iobidze26@gmail.com>
2025-10-18 12:27:51 +0400

    Todo list is done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 function App() {
   const [value, setValue]=useState("")
   const [items, setItems]=useState([])
+  const [list, linedList]=useState([])
   const handleSaveItem=()=>{
     console.log("User wants to save something", value)
     if(value===""){
@@ -16,23 +17,40 @@ function App() {
     setValue(event.target.value)
   }
   const handleDelete=(indexToDelete)=>{
-    const filteredItems=items.filter(
-      (item, index)=>index!==indexToDelete)
-    setItems(filteredItems)
+    setItems(items.filter((item, index)=>index!==indexToDelete))
+    const newChecked = list.filter((i)=>i!==indexToDelete).map((i)=>(i>indexToDelete ? i-1 : i))
+    linedList(newChecked)
+  }
+  const handleCheckbox=(indexToChack)=>{
+    if (list.includes(indexToChack)) {
+      linedList(list.filter((i) => i !==indexToChack));
+    } else {
+      linedList([...list,indexToChack]);
+    }
   }
   return<>
+    <div className="container">
+    <h1>✨ To-Do List ✨</h1>
+    <div className="row">
     <input type="text" 
     placeholder="Enter text" 
+    className="input-text"
     value={value} 
     onChange={handleInputChange}/>
-    <button onClick={handleSaveItem}>Save</button>
-    <ul>
+    <button onClick={handleSaveItem} className="add">Save</button>
+    </div>
       {items.map((item, index)=>{
         return(
-          <li className={index%2===0? "green-text": "red-text"}>{item}<button onClick={()=>handleDelete(index)}>Delete</button></li>
+            <div className="list-item">
+              <div className="list-left">
+              <input type="checkbox" checked={list.includes(index)} onClick={()=>handleCheckbox(index)}/>
+              <span className={list.includes(index) ? "checked" : ""}>{item}</span>
+              </div>
+            <button className="delete-btn"onClick={()=>handleDelete(index)}>❌</button>
+            </div>
         )
       })}
-    </ul>
+    </div>
   </>
 }
 
